22ef2620c13efef3290c63cf77ad4f546b96602e
Florent Ramière <florent@ramiere.com>
2015-04-06 23:43:00 +0200

    step3:expr

--- src/main/java/fr/devoxx/grandmere/GrandMereVisitor.java
+++ src/main/java/fr/devoxx/grandmere/GrandMereVisitor.java
@@ -7,6 +7,51 @@ public class GrandMereVisitor extends GrandMereBaseVisitor<String> {
 
     @Override
     public String visitAtomExpr(GrandMereParser.AtomExprContext ctx) {
-        return ctx.getText();
+        return "Double.valueOf(" + ctx.getText() + ")";
+    }
+
+    @Override
+    public String visitUnaryMinusExpr(GrandMereParser.UnaryMinusExprContext ctx) {
+        return "-" + visit(ctx.expr());
+    }
+
+    @Override
+    public String visitUnaryPlusExpr(GrandMereParser.UnaryPlusExprContext ctx) {
+        return "+" + visit(ctx.expr());
+    }
+
+    @Override
+    public String visitHighPriorityOperationExpr(GrandMereParser.HighPriorityOperationExprContext ctx) {
+        return "(" + visit(ctx.expr(0)) + ")." + visit(ctx.high_priority_operation()) + "(" + visit(ctx.expr(1)) + ")";
+    }
+
+    @Override
+    public String visitHigh_priority_operation(GrandMereParser.High_priority_operationContext ctx) {
+        switch(ctx.getText()) {
+            case "*":
+                return "multiply";
+            case "/":
+                return "divite";
+            default:
+                throw new RuntimeException("Could not find operation " + ctx.getText());
+        }
+    }
+
+    @Override
+    public String visitLowPriorityOperationExpr(GrandMereParser.LowPriorityOperationExprContext ctx) {
+        return "(" + visit(ctx.expr(0)) + ")." + visit(ctx.low_priority_operation()) + "(" + visit(ctx.expr(1)) + ")";
+    }
+
+    @Override
+    public String visitLow_priority_operation(GrandMereParser.Low_priority_operationContext ctx) {
+        switch(ctx.getText()) {
+            case "+":
+                return "add";
+            case "-":
+                return "minus";
+            default:
+                throw new RuntimeException("Could not find operation " + ctx.getText());
+
+        }
     }
 }
--- src/test/java/fr/devoxx/grandmere/GrandMereVisitorTest.java
+++ src/test/java/fr/devoxx/grandmere/GrandMereVisitorTest.java
@@ -12,7 +12,27 @@ public class GrandMereVisitorTest {
 
     @Test
     public void should_tranform_atom() {
-        assertThat(transform("1")).isEqualTo("1");
+        assertThat(transform("1")).isEqualTo("Double.valueOf(1)");
+    }
+
+    @Test
+    public void should_tranform_unary_minus() {
+        assertThat(transform("-2")).isEqualTo("-Double.valueOf(2)");
+    }
+
+    @Test
+    public void should_transform_high_priority_operation() {
+        assertThat(transform("-2*3")).isEqualTo("(-Double.valueOf(2)).multiply(Double.valueOf(3))");
+    }
+
+    @Test
+    public void should_transform_low_priority_operation() {
+        assertThat(transform("-2-3")).isEqualTo("(-Double.valueOf(2)).minus(Double.valueOf(3))");
+    }
+
+    @Test
+    public void should_transform_high_priority_operation_before_low_priority() {
+        assertThat(transform("-2-3*4")).isEqualTo("(-Double.valueOf(2)).minus((Double.valueOf(3)).multiply(Double.valueOf(4)))");
     }
 
     private String transform(String input) {

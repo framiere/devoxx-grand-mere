a88627e6610d4aecee7eb84dba7cb32f41dbe171
Florent Ramière <florent@ramiere.com>
2015-04-07 01:21:43 +0200

    step6:perf

--- src/test/java/fr/devoxx/grandmere/ExcelTest.java
+++ src/test/java/fr/devoxx/grandmere/ExcelTest.java
@@ -1,51 +1,65 @@
 package fr.devoxx.grandmere;
 
+import com.carrotsearch.junitbenchmarks.AbstractBenchmark;
+import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
 import com.google.common.io.Resources;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.google.common.base.Throwables.propagate;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ExcelTest {
-    @Test
-    public void should_load_excel() {
-        Workbook workbook = getWorkbook("perf.xlsx");
-        Sheet perf = workbook.getSheetAt(0);
-        for (int rowId = perf.getFirstRowNum(); rowId < perf.getLastRowNum(); rowId++) {
-            Row row = perf.getRow(rowId);
-            for (int column = 0; column < row.getLastCellNum(); column++) {
-                Cell cell = row.getCell(column);
-                System.out.println(cell);
-                assertThat(cell).isNotNull();
-            }
-        }
+public class ExcelTest extends AbstractBenchmark {
+    private static Workbook workbook;
+    private static CreationHelper creationHelper;
+
+    @BeforeClass
+    public static void loadWorkBook() {
+        workbook = getWorkbook("perf.xlsx");
+        creationHelper = workbook.getCreationHelper();
     }
 
+    @BenchmarkOptions(benchmarkRounds = 1000, warmupRounds = 100, concurrency = 8)
+    @Test
+    public void should_load_excel() {
+        Sheet perf = workbook.getSheetAt(0);
+        List<String> formulas = new ArrayList<>();
+        for (int rowId = perf.getFirstRowNum(); rowId < perf.getLastRowNum(); rowId++) {
+            Row row = perf.getRow(rowId);
+            for (int column = 0; column < row.getLastCellNum(); column++) {
+                String formula = row.getCell(column).toString();
+                assertThat(formula).isNotNull();
+                formulas.add(formula);
+            }
+        }
+        assertThat(formulas).hasSize(189);
+    }
+
+    @BenchmarkOptions(benchmarkRounds = 1000, warmupRounds = 100, concurrency = 8)
     @Test
     public void should_load_execute() {
-        Workbook workbook = getWorkbook("perf.xlsx");
-        FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
+        FormulaEvaluator evaluator = creationHelper.createFormulaEvaluator();
 
         Sheet perf = workbook.getSheetAt(0);
         for (int rowId = perf.getFirstRowNum(); rowId < perf.getLastRowNum(); rowId++) {
             Row row = perf.getRow(rowId);
             for (int column = 0; column < row.getLastCellNum(); column++) {
-                String cell = cell(row.getCell(column), evaluator);
-                System.out.println(cell);
-                assertThat(cell).describedAs("Cell " + rowId + ":" + column).isNotNull();
+                assertThat(cell(row.getCell(column), evaluator)).isNotNull();
             }
         }
     }
 
     private String cell(Cell cell, FormulaEvaluator evaluator) {
-        switch(cell.getCellType()) {
+        switch (cell.getCellType()) {
             case Cell.CELL_TYPE_FORMULA:
                 return evaluator.evaluate(cell).formatAsString();
             case Cell.CELL_TYPE_NUMERIC:
